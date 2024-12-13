1829fa33ea944568a2bdfb820384f649c0429d5c
AbidKhan01ak <akkhan.abid.01@gmail.com>
2024-12-13 06:44:46 +0000

    Audio from device functionality checking

--- script.js
+++ script.js
@@ -43,17 +43,6 @@ async function captureDeviceAudio(){
 
 } 
 captureDeviceAudio();
-recognition.addEventListener("result", (e) => {
-  const transcript = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join("");
-  p.textContent = transcript;
-  if (e.results[0].isFinal) {
-    p = document.createElement("p");
-    words.appendChild(p);
-  }
-});
 
 recognition.addEventListener("end", recognition.start);
 
